5c4fbefe63bb868d22fae540fd9a0d21a607a828
d4nky <vertix08@gmail.com>
2022-01-20 13:36:46 +0100

    added full functionality

--- js/main.js
+++ js/main.js
@@ -1,28 +1,49 @@
 const btn = document.getElementById('submit-btn')
 
+
 btn.addEventListener('click', (e) =>{
     e.preventDefault();
-    var color = document.getElementById('color-selector').value
+    var dateColor = document.getElementById('date-color-selector').value
+    var qrColor = document.getElementById('qr-color-selector').value
     const name = document.getElementById('name').value
     const id = document.getElementById('id').value
-    if(id.length == 0){
+    const passportInput = document.getElementById('passport-input').value
+    const passport = document.getElementById('passport')
+    if(name.length == 0){
         alert('Please enter your id!')
     }
-    else if(color == 0){
-        alert('Please choose a color!')
-    }
-    else if(name.length == 0){
+    else if(id.length == 0){
         alert('Please enter your name!')
     }
+    else if(passport.length == 0){
+        alert('Please enter your passport!')
+    }
     else{
-        var qrcode = new QRCode(document.getElementById("qrcode-2"), {
-            text: `${name}+${id}`,
+        passport.innerHTML = `${passportInput}`
+        var qrcode = new QRCode(document.getElementById("qrcode"), {
+            text: `${id}_${name}`,
             width: 128,
             height: 128,
-            colorDark : `${color}`,
+            colorDark : `${qrColor}`,
             colorLight : "#ffffff",
             correctLevel : QRCode.CorrectLevel.H
         });
+        date.style.color = dateColor
     }
 }
 )
+
+var date = document.getElementById('date')
+function timer(){
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var h = today.getHours();
+var min = today.getMinutes();
+var sec = today.getSeconds();
+
+today = `${mm}-${dd} ${h}:${min}:${sec}`;
+date.textContent = today;
+setTimeout(timer, 1000)
+}
+timer();
